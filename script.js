0eeb6b647c3eab72bc44f3b10f015f63fdefbdc4
AdarshKumarBhardwaj <129866424+AdarshKumarBhardwaj@users.noreply.github.com>
2023-07-31 17:13:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,63 +2,7 @@ const getcolor = () => {
     const randomnumber = Math.floor(Math.random() * 16777215);
     const randomcode = "#" + randomnumber.toString(16);
     document.body.style.backgroundColor = randomcode;
-    document.getElementById("color-code").innerHTML=randomcode;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    document.getElementById("color-code").innerHTML=randomcode;    
 }
 document.getElementById("btn").addEventListener("click", getcolor)
 getcolor();
